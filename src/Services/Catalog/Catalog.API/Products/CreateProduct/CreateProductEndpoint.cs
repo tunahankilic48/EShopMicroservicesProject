50523bda821eb3e02c21acb73772727dd46920a2
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-14 20:31:09 +0300

    Create Product feature was created

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResponse(Guid id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                CreateProductCommand command = request.Adapt<CreateProductCommand>();
+                CreateProductResult result = await sender.Send(command);
+                CreateProductResponse response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
+        }
+    }
+}
