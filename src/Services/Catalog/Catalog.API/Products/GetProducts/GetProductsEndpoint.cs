72e3bcc1a895393ca73c7618553526810c4fe873
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-14 20:56:19 +0300

    Get products Feature created.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    //public record GetProductsRequest() : IQuery<GetProductsResult>;
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                GetProductsResult result = await sender.Send(new GetProductsQuery());
+                GetProductsResponse response = result.Adapt<GetProductsResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+}
