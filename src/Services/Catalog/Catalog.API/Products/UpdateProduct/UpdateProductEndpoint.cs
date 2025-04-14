e5c0451980d759de1a153c71e72c9f8c67e9d0ba
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-14 22:31:20 +0300

    Update Product Feature created.

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -1,6 +1,24 @@
 ﻿namespace Catalog.API.Products.UpdateProduct
 {
-    public class UpdateProductEndpoint
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                UpdateProductCommand command = request.Adapt<UpdateProductCommand>();
+                UpdateProductResult result = await sender.Send(command);
+                UpdateProductResponse response = result.Adapt<UpdateProductResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("UpdateeProduct")
+                .Produces<UpdateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Update Product")
+                .WithDescription("Update Product");
+        }
     }
 }
