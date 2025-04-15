29ab623947a0ef8941aa305bc3bafd32a5359730
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-15 23:37:16 +0300

    Delete basket feature implimented

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket
+{
+    //public record DeleteBasketRequest(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{userName}", async (string userName, ISender sender) =>
+            {
+                DeleteBasketResult result = await sender.Send(new DeleteBasketCommand(userName));
+                DeleteBasketResponse response = result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteBasket")
+            .Produces<DeleteBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Basket")
+            .WithDescription("Delete Basket");
+        }
+    }
+}
