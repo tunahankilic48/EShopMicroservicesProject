0c70966bc2c125ff6a5845bc0bfafaa9b306535e
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-15 23:27:09 +0300

    Store basket feature implimented

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResponse(string UserName);
+    public class StoreBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                StoreBasketCommand query = request.Adapt<StoreBasketCommand>();
+                StoreBasketResult result = await sender.Send(query);
+                StoreBasketResponse response = result.Adapt<StoreBasketResponse>();
+
+                return Results.Created($"/basket/{response.UserName}", response);
+            })
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Store Basket")
+            .WithDescription("Store Basket");
+        }
+    }
+}
