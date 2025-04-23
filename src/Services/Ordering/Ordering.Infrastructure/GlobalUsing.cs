c1460d3fbf98deca40d3a4dc766ef0621048cb55
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-23 21:26:10 +0300

    interceptor created

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObject;
 global using Ordering.Infrastructure.Data;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Infrastructure.Data.Interceptors;
