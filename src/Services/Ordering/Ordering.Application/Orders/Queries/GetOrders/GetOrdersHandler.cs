65cbffab392d624c80992b9eaff1b7b9be2376b7
Tunahan Kılıç <tunahankilic942@gmail.com>
2025-04-25 23:17:09 +0300

    Get orders feature and pagination implimented

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+    {
+        public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+        {
+            int pageIndex = query.PaginationRequest.PageIndex;
+            int pageSize = query.PaginationRequest.PageSize;
+
+            long totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+            List<Order> orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .OrderBy(o => o.OrderName.Value)
+                .Skip(pageSize * pageIndex)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersResult(new PaginatedResult<OrderDto>(pageIndex, pageSize, totalCount, orders.ToOrderDtoList()));
+        }
+    }
+}
